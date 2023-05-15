a6ee873a5e63b2e44c8ff898e97bcba7b381a220
Christopher Morales <christopher.moraleswebdev@gmail.com>
2023-05-15 15:46:41 -0500

    calculator completed

---
+++ script.js
@@ -0,0 +1,43 @@
+const screenOutput = document.querySelector('.screen-output')
+const number = document.querySelector('.number')
+const operation = document.querySelector('.operation')
+const clearButton = document.getElementById('clear-btn')
+const allButtons = document.querySelectorAll('.btn')
+const equalsButton = document.getElementById('equals-btn')
+const multiplicationButton = document.getElementById('multiplication-btn')
+const divisionButton = document.getElementById('division-btn')
+const additionButton = document.getElementById('addition-btn')
+const subtractionButton = document.getElementById('subtraction-btn')
+
+
+
+// This displays what has been clicked
+allButtons.forEach(button => {
+      button.addEventListener('click', () => {
+        screenOutput.innerText += button.textContent;
+      });
+});
+
+// Clear button will run clearScreen function when its clicked
+clearButton.addEventListener('click', clearScreen)
+// Equals button
+equalsButton.addEventListener('click', calculateResult);
+
+
+
+function calculateResult() {
+      const whatsShowing = screenOutput.innerText;
+      if (whatsShowing.includes('=')) {
+        let result = eval(whatsShowing.replace('=', ''));
+        screenOutput.innerText = result;
+      } else {
+        // Wait for the equal sign to be pressed
+      }
+}
+    
+
+function clearScreen() {
+      screenOutput.innerText = '';
+}
+
+    
